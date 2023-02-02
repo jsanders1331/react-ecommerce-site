ffebb58f7ba18fb8447d04b054acfd77575c97ad
james.sanders1337@gmail.com <james.sanders1337@gmail.com>
2023-02-02 15:53:20 +1000

    added: redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
 import Navbar from "./scenes/global/Navbar";
 import Home from "./scenes/home/Home";
+
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -18,8 +20,9 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <ScrollToTop />
         <Navbar />
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="item/:itemId" element={<ItemDetails />} />
